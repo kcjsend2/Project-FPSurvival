31cef9a7622379b1cbf60f76970c15318c2cf65e
kcjsend2 <kcjsend2@naver.com>
2023-05-04 20:17:42 +0900

    엔진 위치 변경(드라이브 용량 이슈)

--- Source/FPSurvival/TStateMachine.cpp
+++ Source/FPSurvival/TStateMachine.cpp
@@ -3,33 +3,34 @@
 
 #include "TStateMachine.h"
 
-template<typename T>
-TStateMachine<T>::TStateMachine(T InitialState)
+template<typename T, typename O>
+TStateMachine<T, O>::TStateMachine(T InitialState, O* InitialOwner)
 {
 	CurrentState = InitialState;
 	PrevState = InitialState;
+	Owner = InitialOwner;
 }
 
-template<typename T>
-void TStateMachine<T>::AddTransitionFunc(std::function<void(AFPSurvivalCharacter&, bool)> TransitionFunc, T StateFrom, T StateTo)
+template<typename T, typename O>
+void TStateMachine<T, O>::AddTransitionFunc(std::function<bool(O&)> TransitionFunc, T StateFrom, T StateTo)
 {
 	TransitionFuncTable.Add(TPair<T, T>(StateFrom, StateTo), TransitionFunc);
 }
 
-template<typename T>
-void TStateMachine<T>::AddInitFunc(std::function<void(AFPSurvivalCharacter&)> InitFunc, T State)
+template<typename T, typename O>
+void TStateMachine<T, O>::AddInitFunc(std::function<void(O&)> InitFunc, T State)
 {
 	StateInitFuncTable.Add(State, InitFunc);
 }
 
-template<typename T>
-void TStateMachine<T>::AddEndFunc(std::function<void(AFPSurvivalCharacter&)> EndFunc, T State)
+template<typename T, typename O>
+void TStateMachine<T, O>::AddEndFunc(std::function<void(O&)> EndFunc, T State)
 {
 	StateEndFuncTable.Add(State, EndFunc);
 }
 
-template<typename T>
-bool TStateMachine<T>::CheckStateTransition(T StateTo)
+template<typename T, typename O>
+bool TStateMachine<T, O>::CheckStateTransition(T StateTo)
 {
 	if(CurrentState == StateTo)
 		return false;
@@ -40,13 +41,16 @@ bool TStateMachine<T>::CheckStateTransition(T StateTo)
 	if(!TransitionFuncTable[TPair<T, T>(CurrentState, StateTo)])
 		return false;
 	
-	if(TransitionFuncTable[TPair<T, T>(CurrentState, StateTo)].Execute())
+	if(TransitionFuncTable[TPair<T, T>(CurrentState, StateTo)]())
 	{
 		PrevState = CurrentState;
 		CurrentState = StateTo;
+
+		if(StateEndFuncTable[PrevState])
+			StateEndFuncTable[PrevState](Owner);
 		
-		StateEndFuncTable[PrevState].ExecuteIfBound();
-		StateInitFuncTable[CurrentState].ExecuteIfBound();
+		if(StateInitFuncTable[CurrentState])
+			StateInitFuncTable[CurrentState](Owner);
 		return true;
 	}
 	return false;
--- Source/FPSurvival/TStateMachine.h
+++ Source/FPSurvival/TStateMachine.h
@@ -5,25 +5,25 @@
 #include <type_traits>
 
 // T는 반드시 enum이어야 함
-template<typename T>
+template<typename T, typename O>
 class TStateMachine
 {
 	static_assert(std::is_enum_v<T>, "T must be enum type.");
 	
 public:
-	TStateMachine() = delete;
-	explicit TStateMachine(T InitialState);
+	TStateMachine() {};
+	explicit TStateMachine(T InitialState, O* InitialOwner);
 
 	// 매 프레임 TransitionFunc를 검사하여 CurrentState가 StateFrom일 때,
 	// TransitionFunc의 반환값이 True이면 StateTo로 CurrentState를 변경
 	// 하나의 StateFrom에서 다수의 StateTo에 대한 동일한, 혹은 포함 관계에 있는 TransitionFunc를 넣지 말 것
-	void AddTransitionFunc(std::function<void(AFPSurvivalCharacter&, bool)> TransitionFunc, T StateFrom, T StateTo);
+	void AddTransitionFunc(std::function<bool(O&)> TransitionFunc, T StateFrom, T StateTo);
 
 	// CurrentState가 State로 바뀌었을 때 수행할 InitFunc을 추가하는 함수
-	void AddInitFunc(std::function<void(AFPSurvivalCharacter&)> InitFunc, T State);
+	void AddInitFunc(std::function<void(O&)> InitFunc, T State);
 
 	// CurrentState가 State에서 다른 값으로 변경될 때 수행할 EndFunc을 추가하는 함수
-	void AddEndFunc(std::function<void(AFPSurvivalCharacter&)> EndFunc, T State);
+	void AddEndFunc(std::function<void(O&)> EndFunc, T State);
 
 	// CurrentState에서 StateTo에 대한 검사
 	bool CheckStateTransition(T StateTo);
@@ -34,7 +34,9 @@ public:
 private:
 	T CurrentState;
 	T PrevState;
-	TMap<TPair<T, T>, std::function<bool(AFPSurvivalCharacter&)>> TransitionFuncTable;
-	TMap<T, std::function<void(AFPSurvivalCharacter&)>> StateInitFuncTable;
-	TMap<T, std::function<void(AFPSurvivalCharacter&)>> StateEndFuncTable;
+	TMap<TPair<T, T>, std::function<bool(O&)>> TransitionFuncTable;
+	TMap<T, std::function<void(O&)>> StateInitFuncTable;
+	TMap<T, std::function<void(O&)>> StateEndFuncTable;
+
+	O* Owner = nullptr;
 };
